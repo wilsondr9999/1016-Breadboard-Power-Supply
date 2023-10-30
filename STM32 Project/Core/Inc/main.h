9ad2ebbd134a4b0403cd0acd2add3ec2daacd0f2
github@thedanzone.net <github@thedanzone.net>
2023-10-29 23:34:22 -0500

    v2.0.0.0
    1016 Breadboard Power Supply v2
    
    - Change FSW connection to the Vout on all regulators
    - Change regulator to TPS62130A so that PG pin goes low on thermal shutdown and UVLO
    - Change PG pullup to be a resistor at the regulator vice pullup within STM32 so that it can be actively discharged
    - Remove resistor across rail outputs for discharging to improve efficiency
    - Change board dimensions to correct span
    - Change pinout of STM32 for better routing
    - Change Tag-Connect footprint to momentary connection (no legs)
    - Make sure Tag-Connect footprint doesn't have solder paste layer
    - Added heat sinks on bottom under rail regulators, added thermal vias

--- STM32 Project/Core/Inc/main.h
+++ STM32 Project/Core/Inc/main.h
@@ -57,32 +57,32 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_Error_Pin GPIO_PIN_0
-#define LED_Error_GPIO_Port GPIOA
-#define LED_Output_Pin GPIO_PIN_1
-#define LED_Output_GPIO_Port GPIOA
-#define LED_5VT_Pin GPIO_PIN_2
-#define LED_5VT_GPIO_Port GPIOA
-#define LED_5VB_Pin GPIO_PIN_3
-#define LED_5VB_GPIO_Port GPIOA
-#define LED_3V3T_Pin GPIO_PIN_4
-#define LED_3V3T_GPIO_Port GPIOA
-#define LED_3V3B_Pin GPIO_PIN_5
+#define PGood_RailB_Pin GPIO_PIN_0
+#define PGood_RailB_GPIO_Port GPIOA
+#define LED_3V3B_Pin GPIO_PIN_1
 #define LED_3V3B_GPIO_Port GPIOA
-#define Btn_Config_Pin GPIO_PIN_6
-#define Btn_Config_GPIO_Port GPIOA
-#define Btn_OutputEn_Pin GPIO_PIN_7
-#define Btn_OutputEn_GPIO_Port GPIOA
-#define Select_VRailB_Pin GPIO_PIN_8
-#define Select_VRailB_GPIO_Port GPIOA
+#define LED_5VB_Pin GPIO_PIN_2
+#define LED_5VB_GPIO_Port GPIOA
+#define LED_Output_Pin GPIO_PIN_3
+#define LED_Output_GPIO_Port GPIOA
+#define LED_Error_Pin GPIO_PIN_4
+#define LED_Error_GPIO_Port GPIOA
+#define LED_3V3T_Pin GPIO_PIN_5
+#define LED_3V3T_GPIO_Port GPIOA
+#define LED_5VT_Pin GPIO_PIN_6
+#define LED_5VT_GPIO_Port GPIOA
+#define Output_Enable_Pin GPIO_PIN_7
+#define Output_Enable_GPIO_Port GPIOA
+#define PGood_RailT_Pin GPIO_PIN_8
+#define PGood_RailT_GPIO_Port GPIOA
 #define Select_VRailT_Pin GPIO_PIN_11
 #define Select_VRailT_GPIO_Port GPIOA
-#define Output_Enable_Pin GPIO_PIN_12
-#define Output_Enable_GPIO_Port GPIOA
-#define PGood_RailB_Pin GPIO_PIN_6
-#define PGood_RailB_GPIO_Port GPIOB
-#define PGood_RailT_Pin GPIO_PIN_7
-#define PGood_RailT_GPIO_Port GPIOB
+#define Btn_Config_Pin GPIO_PIN_12
+#define Btn_Config_GPIO_Port GPIOA
+#define Btn_OutputEn_Pin GPIO_PIN_6
+#define Btn_OutputEn_GPIO_Port GPIOB
+#define Select_VRailB_Pin GPIO_PIN_7
+#define Select_VRailB_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
